eee2182512e83371133d4df8d406727bbf1f7af8
kanpon <kanpon@yeah.net>
2024-06-23 22:38:25 +0800

    feat: 页面UI调整

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,11 @@
+import dayjs from "dayjs";
 import "./style/index.scss";
 
 function Footer() {
-  return <div className="footer">Arco Design Pro</div>;
+  return <div className="footer">
+    <div className="footer-left">-</div>
+    <div className="footer-right">{dayjs().format("YYYY-MM-DD HH:mm:ss")}</div>
+  </div>;
 }
 
 export default Footer;
